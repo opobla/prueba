d6ac8e5336293985fcc9c035c54d01788baeb519
Óscar García <oscar.gpoblacion@uah.es>
2014-04-10 15:27:44 +0200

    Corregir archivos de cabecera y formatear salida

--- hilos.c
+++ hilos.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <pthread.h>
-#include <cruz.h>
-#include <estrella.h>
+#include "cruz.h"
+#include "estrella.h"
 
 
 int main() {
-    printf("Bienvenidos a mi programa de hilos");
+    printf("Bienvenidos a mi programa de hilos\n");
     pinta_cruz();
     pinta_estrella();
 }
